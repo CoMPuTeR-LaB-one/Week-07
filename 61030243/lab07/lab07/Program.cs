d91bb73c70a9c125a6ba85f7c9539ab31ae7c4f5
WidsawaD <safefire@gmail.com>
2019-10-18 15:21:02 +0700

    การเข้าถึง fields แบบ public2

--- 61030243/lab07/lab07/Program.cs
+++ 61030243/lab07/lab07/Program.cs
@@ -10,6 +10,20 @@ namespace lab07
     {
         static void Main(string[] args)
         {
+            Student su = new Student();
+            su.Name = "Student Name";
+            su.ID = "12345678";
+            su.GPA = 7.5f;
+            Console.WriteLine("Student name : " + su.Name);
+            Console.WriteLine("Student ID   : " + su.ID);
+            Console.WriteLine("Student GPA  : " + su.GPA);
+            Console.ReadLine();
+        }
+        class Student
+        {
+            private string Name;
+            private string ID;
+            private float GPA;
         }
     }
 }
